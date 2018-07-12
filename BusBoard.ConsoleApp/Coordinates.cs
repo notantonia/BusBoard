1c6df2cb6f64743393f4a29906538f7a416d235a
Antonia Siu <Antonia.Siu@softwire.com>
2018-07-12 16:02:41 +0100

    Show coordinates of softwire

---
+++ BusBoard.ConsoleApp/Coordinates.cs
@@ -0,0 +1,8 @@
+﻿namespace BusBoard.ConsoleApp
+{
+    class Coordinates
+    {
+        public float longitude { get; set; }
+        public float latitude { get; set; }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -8,28 +8,43 @@ using RestSharp;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
             // TODO: Add API keys
-            var client = new RestClient("https://api.tfl.gov.uk");
+            var TFLClient = new RestClient("https://api.tfl.gov.uk");
 
             var request = new RestRequest("StopPoint/{id}/Arrivals", Method.GET);
             request.AddUrlSegment("id", "490008660N");
 
-            IRestResponse<List<BusPrediction>> response = client.Execute<List<BusPrediction>>(request);
+            IRestResponse<List<BusPrediction>> response = TFLClient.Execute<List<BusPrediction>>(request);
             foreach (var prediction in response.Data)
             {
                 Console.WriteLine(
                     "Bus: " + prediction.lineName +
                     "\nTo: " + prediction.destinationName +
-                    "\nIn: " + Math.Round((double) prediction.timeToStation/60) + " minute(s)\n");
+                    "\nIn: " + Math.Round((double)prediction.timeToStation / 60) + " minute(s)\n");
             }
-            
+
+            Coordinates coordinates = GetCoordinates("NW5 1TL");
+            Console.WriteLine(coordinates.longitude + ", " + coordinates.latitude);
+
             Console.Read();
+        }
+
+        static Coordinates GetCoordinates(string postCodeName)
+        {
+            var postCodeClient = new RestClient("https://api.postcodes.io");
+
+            var request = new RestRequest("postcodes/{name}", Method.GET);
+            request.AddUrlSegment("name", postCodeName);
+
+            IRestResponse<PostCodeResponse> response = postCodeClient.Execute<PostCodeResponse>(request);
+
+            return response.Data.result;
         }
     }
 }
