81e9dc4d119a973e197e16e1525a16db4f392156
Antonia Siu <Antonia.Siu@softwire.com>
2018-07-12 15:23:12 +0100

    Show buses arriving at LSR as string

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace BusBoard.ConsoleApp
 {
@@ -10,6 +12,19 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            // TODO: Add API keys
+            var client = new RestClient("https://api.tfl.gov.uk");
+
+            var request = new RestRequest("StopPoint/{id}/Arrivals", Method.GET);
+            request.AddUrlSegment("id", "490008660N");
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            Console.Write(content);
+            Console.Read();
+        }
     }
-  }
 }
